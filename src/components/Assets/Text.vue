21eac74acca9d38b6eb759cd022ffad29840b880
UniTaeMin <onnu0802@naver.com>
2021-06-04 23:10:08 +0900

    Add Text Component

---
+++ src/components/Assets/Text.vue
@@ -0,0 +1,57 @@
+<template>
+  <i
+    v-if="textType == 'i'"
+    :style="{fontSize: fontSize, display: displayType, margin: margin, justifyContent:justifyContent, alignItems:alignItems, fontFamily:fontFamily, textAlign:textAlign}"
+  >
+    <slot />
+  </i>
+  <span
+    v-else
+    :style="{fontSize: fontSize, display: displayType, margin: margin, justifyContent:justifyContent, alignItems:alignItems, fontFamily:fontFamily, textAlign: textAlign}"
+  >
+    <slot />
+  </span>
+</template>
+
+<script>
+export default {
+  name: "TextComponent",
+  props: {
+    margin: {
+      type: String,
+      default: "0"
+    },
+    displayType: {
+      type: String,
+      default: "block"
+    },
+    fontSize: {
+      type: String,
+      default: "16px"
+    },
+    justifyContent: {
+      type: String,
+      default: "normal"
+    },
+    alignItems: {
+      type: String,
+      default: "normal"
+    },
+    textType: {
+      type: String,
+      default: ""
+    },
+    textAlign: {
+      type: String,
+      default: "center"
+    },
+    fontFamily: {
+      type: String,
+      default: "NanumSquareEB"
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>

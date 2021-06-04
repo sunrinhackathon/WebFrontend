a7417c3d89727a62d88b4db53a6ff142a46cca2d
UniTaeMin <onnu0802@naver.com>
2021-06-04 23:09:57 +0900

    Add Timer Component

---
+++ src/components/Home/Timer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="timer">
+    <div v-for="data in timeArray" :key="data.timeName">
+      <div>{{data.data}}</div>
+      <TextComponent
+        textType="i"
+        display="flex"
+        margin="0 auto"
+        justifyContent="center"
+        fontSize="30px"
+      >{{data.timeName}}</TextComponent>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Timer",
+  components: {},
+  props: {
+    timeArray: Array
+  },
+  created() {}
+};
+</script>
+
+<style scoped>
+.timer {
+  font-size: 94px;
+  color: white;
+  text-shadow: -2px 0 black, 0 2px black, 2px 0 black, 0 -2px black;
+  display: flex;
+  height: fit-content;
+}
+.timer > * {
+  margin: 0px 26px;
+}
+</style>

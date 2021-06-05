9e428d3e1a6058143c5543272917aea748e17757
UniTaeMin <onnu0802@naver.com>
2021-06-05 09:38:42 +0900

    Refactor Landing Page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,104 +1,95 @@
 <template>
   <div class="home">
     <section class="home__banner">
-      <!-- 배너 이미지랑 타이머 따로 컴퍼넌트화 -->
-      <div class="home__banner_image">
-        <img src="@/assets/images/developer.svg" />
-        <img src="@/assets/images/designer.svg" />
+      <div class="home__banner__images">
+        <img src="@/assets/images/developer.svg" class="home__banner__image__developer" />
+        <img class="home__banner__logo" src="@/assets/images/title.svg" alt="제7회 선린톤 로고 이미지" />
+        <img src="@/assets/images/designer.svg" class="home__banner__image__designer" />
       </div>
-      <img class="home__banner__logo" src="@/assets/images/title.svg" alt="제7회 선린톤 로고 이미지" />
-      <img src="@/assets/images/timer.svg" alt="선린톤 타이머 이미지" width="150px" />
-
+      <img src="@/assets/images/timer.svg" alt="선린톤 타이머 이미지" class="home__banner__timer" />
       <TimerComponent :timeArray="time"></TimerComponent>
-
       <button class="home__banner__button">예선 지원하러 가기</button>
-
       <div class="home__banner__bottom"></div>
     </section>
     <article class="home__notice">
       <div style="font-size: 38px">대회안내</div>
-      <div style="width: 32px; height: 4px; background-color: #102765; margin:60px 0px "></div>
-      <div style="margin-bottom: 80px">
-        <div style="font-size: 44px; text-align: center;">선린 해커톤?</div>
-        <div
-          style="text-align:center; font-size: 28px; font-family:'NanumSquareR'; margin: 36px 0px "
-        >
+      <LineComponent width="32px" height="4px" backgroundColor="#102765" margin="60px 0px"></LineComponent>
+      <section>
+        <TextComponent fontSize="44px">선린 해커톤?</TextComponent>
+        <TextComponent fontSize="28px" fontFamily="NanumSquareR" margin="36px 0px">
           선린 해커톤은 제한 시간 안에 소프트웨어를 개발하는 대회입니다
           <br />정해진 주제에 맞춰 하루만에 앱이나 게임을 개발해보세요!
-        </div>
-      </div>
-      <div style="margin-bottom: 80px">
-        <div style="font-size: 44px;  text-align: center;">예선 관련 안내</div>
-        <div
-          style="text-align:center; font-size: 28px; font-family:'NanumSquareR';  margin: 36px 0px "
-        >
+        </TextComponent>
+      </section>
+      <section>
+        <TextComponent fontSize="44px">예선 관련 안내</TextComponent>
+        <TextComponent fontSize="28px" fontFamily="NanumSquareR" margin="36px 0px">
           예선 통과 인원 : 80명 (게임 앱 분야 10명 / 생활 앱 분야 10명)
           <br />심사 기준 : 포트폴리오 기반 개인 역량, 팀 능력
           <br />결과 발표 : 7/12 (월) 18:00
-        </div>
-      </div>
-      <div style="margin-bottom: 80px">
-        <div style="font-size: 44px;  text-align: center;">본선 관련 안내</div>
-        <div
-          style="text-align:center; font-size: 28px; font-family:'NanumSquareR';  margin: 36px 0px "
-        >
+        </TextComponent>
+      </section>
+      <section>
+        <TextComponent fontSize="44px">본선 관련 안내</TextComponent>
+        <TextComponent fontSize="28px" fontFamily="NanumSquareR" margin="36px 0px">
           개발 : 7/17(토) 8:00 ~ 7/18(일) 8:00
           <br />발표 및 심사 : 7/18(일) 19:00
           <br />결과 발표 : 당일 발표
-        </div>
-      </div>
-      <div style="margin-bottom: 80px">
-        <div style="font-size: 44px;  text-align: center;">주제 공개 방법</div>
-        <div
-          style="text-align:center; font-size: 28px; font-family:'NanumSquareR';  margin: 36px 0px "
-        >
+        </TextComponent>
+      </section>
+      <section>
+        <TextComponent fontSize="44px">주제 공개 방법</TextComponent>
+        <TextComponent fontSize="28px" fontFamily="NanumSquareR" margin="36px 0px">
           • 일주일 전 대주제 공개, 공개된 주제 세분화하여 팀별 랜덤으로 선택
           <br />◦ EX) Color -> Red, Yellow, Green . . .
-        </div>
-      </div>
-      <div style="width: 32px; height: 4px; background-color: #102765; margin-bottom:106px"></div>
-      <div style="margin-bottom: 80px">
-        <div style="font-size: 44px;  text-align: center;">온라인 진행</div>
-        <div
-          style="text-align:center; font-size: 28px; font-family:'NanumSquareR';  margin: 36px 0px "
-        >
+        </TextComponent>
+      </section>
+      <LineComponent width="32px" height="4px" backgroundColor="#102765" margin="0px 0px 106px 0px"></LineComponent>
+      <section>
+        <TextComponent fontSize="44px">온라인 진행</TextComponent>
+        <TextComponent fontSize="28px" fontFamily="NanumSquareR" margin="36px 0px">
           이번 해커톤 역시 COVID-19로 인해 온라인으로 진행하게 되었습니다 :(
           <br />대회 진행 및 운영은 마찬가지로 Discord로 진행할 예정이니
           <br />신청자 분들께서는 디스코드 아이디를 미리 준비해주세요!
-        </div>
-
-        <!-- 이미지 박스 -->
-        <div style="background-color: red; width: 150px; height: 150px; margin: 0 auto;"></div>
+        </TextComponent>
+        <img
+          src="@/assets/images/discord.svg"
+          alt="discord image"
+          width="150px"
+          height="150px"
+          style="width:100%; margin :0 auto"
+        />
         <div style="text-align: center">▲ Discord 공식 홈페이지</div>
-      </div>
-
-      <div style="margin-bottom: 80px">
-        <div style="font-size: 44px;  text-align: center;">문의</div>
-        <div
-          style="text-align:center; font-size: 28px; font-family:'NanumSquareR';  margin: 36px 0px "
-        >선린 해커톤 페이스북 페이지 ( @sunrinhackathon )</div>
+      </section>
+      <section>
+        <TextComponent fontSize="44px">문의</TextComponent>
+        <TextComponent
+          fontSize="28px"
+          fontFamily="NanumSquareR"
+          margin="36px 0px"
+        >선린 해커톤 페이스북 페이지 ( @sunrinhackathon )</TextComponent>
         <!-- 이미지 박스 -->
-        <div style="background-color: red; width: 150px; height: 150px; margin: 0 auto;"></div>
+        <img
+          src="@/assets/images/facebook.svg"
+          alt="facebook image"
+          width="180px"
+          height="180px"
+          style="width:100%; margin :0 auto"
+        />
         <div style="text-align: center">▲ 선린톤 페이스북 페이지</div>
-      </div>
-      <div style="margin-bottom: 80px">
-        <div style="font-size: 44px;  text-align: center;">참가팀 발표</div>
-        <div
-          style="text-align:center; font-size: 28px; font-family:'NanumSquareR';  margin: 36px 0px "
-        >예선 이후 공지 예정입니다 :)</div>
-      </div>
-      <div style="margin-bottom: 80px">
-        <div style="font-size: 44px;  text-align: center;">대회 일정</div>
-        <div
-          style="text-align:center; font-size: 28px; font-family:'NanumSquareR';  margin: 36px 0px "
-        >예선 이후 공지 예정입니다 :)</div>
-      </div>
-      <div style="margin-bottom: 80px">
-        <div style="font-size: 44px;  text-align: center;">포인트 교환소</div>
-        <div
-          style="text-align:center; font-size: 28px; font-family:'NanumSquareR';  margin: 36px 0px "
-        >예선 이후 공지 예정입니다 :)</div>
-      </div>
+      </section>
+      <section>
+        <TextComponent fontSize="44px">참가팀 발표</TextComponent>
+        <TextComponent fontSize="28px" fontFamily="NanumSquareR" margin="36px 0px">예선 이후 공지 예정입니다 :)</TextComponent>
+      </section>
+      <section>
+        <TextComponent fontSize="44px">대회 일정</TextComponent>
+        <TextComponent fontSize="28px" fontFamily="NanumSquareR" margin="36px 0px">예선 이후 공지 예정입니다 :)</TextComponent>
+      </section>
+      <section>
+        <TextComponent fontSize="44px">포인트 교환소</TextComponent>
+        <TextComponent fontSize="28px" fontFamily="NanumSquareR" margin="36px 0px">예선 이후 공지 예정입니다 :)</TextComponent>
+      </section>
     </article>
     <footer class="home__footer"></footer>
   </div>
@@ -123,15 +114,28 @@ export default {
 </script>
 
 <style scoped>
+.home__banner__images {
+  width: 100%;
+  margin: 70px 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  height: fit-content;
+}
 .home {
   width: 100%;
+}
+.home__banner__timer {
+  width: 200px;
+  margin-top: -100px;
 }
 .home__banner {
   width: 100%;
   height: calc(100vh + 200px);
   background-color: #113fbd;
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   flex-direction: column;
   position: relative;
@@ -140,6 +144,24 @@ export default {
   color: white;
   font-size: 200px;
   width: 70%;
+}
+.home__banner__image__developer {
+  position: absolute;
+
+  width: 25%;
+
+  transform: rotate(-16deg);
+  top: -20%;
+  left: 7%;
+}
+.home__banner__image__designer {
+  position: absolute;
+
+  width: 22%;
+
+  transform: rotate(5deg);
+  bottom: -34%;
+  right: 4%;
 }
 .home__banner__button {
   color: #102765;
@@ -150,7 +172,7 @@ export default {
   border: 0px;
   box-shadow: #0613aa 10px 10px 3px;
   font-size: 25px;
-  margin-top: 90px;
+  margin-top: 80px;
 }
 .home__banner__bottom {
   height: 80px;
@@ -168,10 +190,23 @@ export default {
   align-items: center;
   flex-direction: column;
 }
+.home__notice > section {
+  margin-bottom: 80px;
+}
 
 .home__footer {
   width: 100%;
   height: 240px;
   background-color: #113fbd;
+}
+
+@media screen and (max-width: 768px) {
+  .home__banner__images {
+    margin: 100px 0px 70px 0px;
+  }
+  .home__banner__timer {
+    width: 140px;
+    margin-top: -50px;
+  }
 }
 </style>

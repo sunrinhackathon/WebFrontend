9000cf5f102cd04e717175512fb4f5e642da7dd9
UniTaeMin <onnu0802@naver.com>
2021-06-05 09:36:09 +0900

    Update Text Component

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# sunrinthon_frontend
+# 선린인터넷고등학교 선린톤 페이지
 
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn build
 ```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/components/Assets/Text.vue
+++ src/components/Assets/Text.vue
@@ -2,12 +2,14 @@
   <i
     v-if="textType == 'i'"
     :style="{fontSize: fontSize, display: displayType, margin: margin, justifyContent:justifyContent, alignItems:alignItems, fontFamily:fontFamily, textAlign:textAlign}"
+    :class="mobileFontSize? 'mobileFontSize':''"
   >
     <slot />
   </i>
   <span
     v-else
     :style="{fontSize: fontSize, display: displayType, margin: margin, justifyContent:justifyContent, alignItems:alignItems, fontFamily:fontFamily, textAlign: textAlign}"
+    :class="mobileFontSize? 'mobileFontSize':''"
   >
     <slot />
   </span>
@@ -48,10 +50,18 @@ export default {
     fontFamily: {
       type: String,
       default: "NanumSquareEB"
+    },
+    mobileFontSize: {
+      type: Boolean
     }
   }
 };
 </script>
 
 <style scoped>
+@media screen and (max-width: 768px) {
+  .mobileFontSize {
+    font-size: 20px !important;
+  }
+}
 </style>

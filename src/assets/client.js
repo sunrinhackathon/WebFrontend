1ea2a55a6c3304d1e744ab2c45e3716a785dacaf
HADMARINE <tsx@kakao.com>
2021-06-15 23:56:14 +0900

    Update api uri

--- src/assets/client.js
+++ src/assets/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // import client from "eqb-request-client";
 
 const api = axios.create({
-  baseURL: "https://dev.api.hackathon.sunrin.dev/v1"
+  baseURL: "https://api.hackathon.sunrin.dev/v1"
 });
 
 export default api;
